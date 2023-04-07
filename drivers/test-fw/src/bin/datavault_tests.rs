07974607f80fa4a642713b3d61aefa59de5a29c5
Kor Nielsen <kor@google.com>
2023-04-06 23:31:32 -0700

    More tests

--- drivers/test-fw/src/bin/datavault_tests.rs
+++ drivers/test-fw/src/bin/datavault_tests.rs
@@ -1,7 +1,7 @@
 #![no_std]
 #![no_main]
 
-use caliptra_drivers::{ColdResetEntry4, DataVault, Mailbox};
+use caliptra_drivers::{ColdResetEntry4, DataVault, Mailbox, Ecc384Signature, Ecc384PubKey};
 use caliptra_registers::mbox::{self};
 use core::mem::size_of;
 use core::slice;
@@ -11,6 +11,11 @@ mod harness;
 #[inline(never)]
 fn test_datavault() {
     let mut dv = DataVault::default();
+
+    dv.set_ldev_dice_signature(&Ecc384Signature::default());
+    dv.set_ldev_dice_pub_key(&Ecc384PubKey::default());
+
+    dv.write_cold_reset_entry4(ColdResetEntry4::FmcEntryPoint, 0xba5e_ba11);
     dv.write_cold_reset_entry4(ColdResetEntry4::FmcEntryPoint, 0xba5e_ba11);
     assert_eq!(dv.fmc_entry_point(), 0xba5e_ba11);
 }
