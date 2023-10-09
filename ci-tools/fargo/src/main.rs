17ebb9c1d5c97b8e98cacb39d4061ef44d3d0ce2
Kor Nielsen <kor@google.com>
2023-10-09 14:28:01 -0700

    fargo checkpoint...

--- ci-tools/fargo/src/main.rs
+++ ci-tools/fargo/src/main.rs
@@ -106,6 +106,17 @@ fn ssh_cmd(host: &RemoteHost, command_str: &str) -> io::Result<()> {
     run_cmd(&mut cmd)
 }
 
+fn ssh_cmd_stdout(host: &RemoteHost, command_str: &str) -> io::Result<Vec<u8>> {
+    let mut cmd = Command::new("ssh");
+    cmd.arg(&host.hostname);
+    cmd.arg("-l");
+    cmd.arg("root");
+    cmd.arg("-p");
+    cmd.arg(host.port.to_string());
+    cmd.arg(command_str);
+    run_cmd_stdout(&mut cmd, None)
+}
+
 fn scp(files: &[PathBuf], host: &RemoteHost, dest: &Path) -> io::Result<()> {
     if files.is_empty() {
         return Ok(())
@@ -124,6 +135,7 @@ fn scp(files: &[PathBuf], host: &RemoteHost, dest: &Path) -> io::Result<()> {
 enum ParseMode {
     WaitingForTest,
     WaitingForTestName,
+    WaitingForTestNameSkip,
     HasTestName,
     TestArgs,
 }
@@ -134,13 +146,27 @@ fn main() {
         panic!("Expected FARGO_REMOTE_HOST environment variable was not set or was invalid. Examples: 'myhost', 'myhost:4022', '192.168.1.2:22', '[2001:0db8::1]:8123'");
     };
 
-    let Ok(Ok(sysroot)) = std::env::var("FARGO_SYSROOT").map(|s| RemoteHost::from_str(s.as_str())) else {
+    let Ok(sysroot) = std::env::var("FARGO_SYSROOT") else {
         panic!("Expected FARGO_SYSROOT environment variable was not set or was invalid.");
     };
 
-    let Ok(Ok(bitstream)) = std::env::var("FARGO_BITSTREAM").map(|s| RemoteHost::from_str(s.as_str())) else {
-        panic!("Expected FARGO_BITSTREAM environment variable was not set or was invalid.");
+    if let Ok(bitstream) = std::env::var("FARGO_BITSTREAM") {
+        ssh_cmd(&remote_host, "mkdir -p /lib/firmware").unwrap();
+        scp(&[bitstream.into()], &remote_host, Path::new("/lib/firmware/caliptra_fpga.bin")).unwrap();
+        ssh_cmd(&remote_host, "bash -c \"echo 0 > /sys/class/fpga_manager/fpga0/flags\"").unwrap();
+        ssh_cmd(&remote_host, "bash -c \"echo 20000000 > /sys/bus/platform/drivers/xilinx_fclk/fclk0/set_rate\"").unwrap();
+        ssh_cmd(&remote_host, "bash -c \"echo caliptra_fpga.bin > /sys/class/fpga_manager/fpga0/firmware\"").unwrap();
+        assert_eq!(String::from_utf8(ssh_cmd_stdout(&remote_host, "cat /sys/class/fpga_manager/fpga0/state").unwrap()).unwrap().trim(), "operating");
     };
+
+    if let Ok(kmod_dir) = std::env::var("FARGO_KMOD") {
+        let kmod_dir = Path::new(&kmod_dir);
+        ssh_cmd(&remote_host, "mkdir -p /tmp/caliptra-kmod").unwrap();
+        scp(&[kmod_dir.join("io_module.ko"),
+              kmod_dir.join("rom_backdoor.ko")], &remote_host, Path::new("/tmp/caliptra-kmod/")).unwrap();
+        ssh_cmd(&remote_host, "insmod /tmp/caliptra-kmod/io_module.ko").unwrap();
+        ssh_cmd(&remote_host, "insmod /tmp/caliptra-kmod/rom_backdoor.ko").unwrap();
+    }
 
     let mut cargo_inv = Command::new("cargo");
     cargo_inv.arg("--config");
@@ -153,12 +179,19 @@ fn main() {
     let mut mode = ParseMode::WaitingForTest;
     let mut test_args = vec![];
     for arg in std::env::args().skip(1) {
+        cargo_inv.arg(arg.clone());
         match mode {
             ParseMode::WaitingForTest if arg == "test" => {
                 cargo_inv.arg("--target=aarch64-unknown-linux-gnu");
                 cargo_inv.arg("--no-run");
                 cargo_inv.arg("--message-format=json");
                 mode = ParseMode::WaitingForTestName;
+            }
+            ParseMode::WaitingForTestNameSkip => {
+                mode = ParseMode::WaitingForTestName;
+            }
+            ParseMode::WaitingForTestName if arg == "-p" => {
+                mode = ParseMode::WaitingForTestNameSkip;
             }
             ParseMode::WaitingForTestName if !arg.starts_with('-') => {
                 test_args.push(arg.clone());
@@ -211,9 +244,10 @@ fn main() {
 
     // TODO: Escape?
     let test_args_str = test_args.join(" ");
+    println!("test_args {}", test_args_str);
     for test_file in test_files {
         let test_name = test_file.file_name().unwrap().to_str().unwrap();
         ssh_cmd(&remote_host, 
-            &format!("bash -c \"CALIPTRA_PREBUILT_FW_DIR=/tmp/fargo-fw CALIPTRA_IMAGE_NO_GIT_REVISION=1 /tmp/fargo-tests/{test_name}\" {test_args_str}")).unwrap();
+            &format!("bash -c \"CPTRA_UIO_NUM=4 CALIPTRA_PREBUILT_FW_DIR=/tmp/fargo-fw CALIPTRA_IMAGE_NO_GIT_REVISION=1 /tmp/fargo-tests/{test_name} {test_args_str}\"")).unwrap();
     }
 }
