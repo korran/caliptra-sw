61d6b273a7fedb51f18fb56f3bb53932e5e1d05d
Kor Nielsen <kor@google.com>
2023-10-09 17:27:49 -0700

    Checkpoint

--- ci-tools/fargo/src/main.rs
+++ ci-tools/fargo/src/main.rs
@@ -1,8 +1,13 @@
-
-use std::io::Read;
-use std::{path::{PathBuf, Path}, net::{SocketAddr, IpAddr}, str::FromStr, fmt::Display, process::{Command, Stdio}, io};
 use caliptra_builder::firmware;
 use cargo_metadata::Message;
+use std::{
+    fmt::Display,
+    io,
+    net::{IpAddr, SocketAddr},
+    path::{Path, PathBuf},
+    process::{Command, Stdio},
+    str::FromStr,
+};
 
 struct RemoteHost {
     hostname: String,
@@ -25,25 +30,25 @@ impl FromStr for RemoteHost {
         };
 
         if let Ok(socket_addr) = SocketAddr::from_str(value) {
-            return Ok(RemoteHost{
+            return Ok(RemoteHost {
                 hostname: format_ip(socket_addr.ip()),
                 port: socket_addr.port(),
-            })
+            });
         }
         if let Ok(ip) = IpAddr::from_str(value) {
-            return Ok(RemoteHost{
+            return Ok(RemoteHost {
                 hostname: format_ip(ip),
                 port: DEFAULT_PORT,
-            })
+            });
         }
 
         if let Some((hostname, port)) = value.split_once(':') {
-            return Ok(RemoteHost{
+            return Ok(RemoteHost {
                 hostname: hostname.into(),
                 port: u16::from_str(port).map_err(|_| ())?,
             });
         }
-        Ok(RemoteHost{
+        Ok(RemoteHost {
             hostname: value.into(),
             port: DEFAULT_PORT,
         })
@@ -75,8 +80,6 @@ pub fn run_cmd_stdout(cmd: &mut Command, input: Option<&[u8]>) -> io::Result<Vec
         ))
     }
 }
-
-
 
 pub fn run_cmd(cmd: &mut Command) -> io::Result<()> {
     let status = cmd.status()?;
@@ -119,7 +122,7 @@ fn ssh_cmd_stdout(host: &RemoteHost, command_str: &str) -> io::Result<Vec<u8>> {
 
 fn scp(files: &[PathBuf], host: &RemoteHost, dest: &Path) -> io::Result<()> {
     if files.is_empty() {
-        return Ok(())
+        return Ok(());
     }
 
     let mut cmd = Command::new("rsync");
@@ -140,7 +143,6 @@ enum ParseMode {
     TestArgs,
 }
 
-
 fn main() {
     let Ok(Ok(remote_host)) = std::env::var("FARGO_REMOTE_HOST").map(|s| RemoteHost::from_str(s.as_str())) else {
         panic!("Expected FARGO_REMOTE_HOST environment variable was not set or was invalid. Examples: 'myhost', 'myhost:4022', '192.168.1.2:22', '[2001:0db8::1]:8123'");
@@ -152,18 +154,49 @@ fn main() {
 
     if let Ok(bitstream) = std::env::var("FARGO_BITSTREAM") {
         ssh_cmd(&remote_host, "mkdir -p /lib/firmware").unwrap();
-        scp(&[bitstream.into()], &remote_host, Path::new("/lib/firmware/caliptra_fpga.bin")).unwrap();
-        ssh_cmd(&remote_host, "bash -c \"echo 0 > /sys/class/fpga_manager/fpga0/flags\"").unwrap();
-        ssh_cmd(&remote_host, "bash -c \"echo 20000000 > /sys/bus/platform/drivers/xilinx_fclk/fclk0/set_rate\"").unwrap();
-        ssh_cmd(&remote_host, "bash -c \"echo caliptra_fpga.bin > /sys/class/fpga_manager/fpga0/firmware\"").unwrap();
-        assert_eq!(String::from_utf8(ssh_cmd_stdout(&remote_host, "cat /sys/class/fpga_manager/fpga0/state").unwrap()).unwrap().trim(), "operating");
+        scp(
+            &[bitstream.into()],
+            &remote_host,
+            Path::new("/lib/firmware/caliptra_fpga.bin"),
+        )
+        .unwrap();
+        ssh_cmd(
+            &remote_host,
+            "bash -c \"echo 0 > /sys/class/fpga_manager/fpga0/flags\"",
+        )
+        .unwrap();
+        ssh_cmd(
+            &remote_host,
+            "bash -c \"echo 20000000 > /sys/bus/platform/drivers/xilinx_fclk/fclk0/set_rate\"",
+        )
+        .unwrap();
+        ssh_cmd(
+            &remote_host,
+            "bash -c \"echo caliptra_fpga.bin > /sys/class/fpga_manager/fpga0/firmware\"",
+        )
+        .unwrap();
+        assert_eq!(
+            String::from_utf8(
+                ssh_cmd_stdout(&remote_host, "cat /sys/class/fpga_manager/fpga0/state").unwrap()
+            )
+            .unwrap()
+            .trim(),
+            "operating"
+        );
     };
 
     if let Ok(kmod_dir) = std::env::var("FARGO_KMOD") {
         let kmod_dir = Path::new(&kmod_dir);
         ssh_cmd(&remote_host, "mkdir -p /tmp/caliptra-kmod").unwrap();
-        scp(&[kmod_dir.join("io_module.ko"),
-              kmod_dir.join("rom_backdoor.ko")], &remote_host, Path::new("/tmp/caliptra-kmod/")).unwrap();
+        scp(
+            &[
+                kmod_dir.join("io_module.ko"),
+                kmod_dir.join("rom_backdoor.ko"),
+            ],
+            &remote_host,
+            Path::new("/tmp/caliptra-kmod/"),
+        )
+        .unwrap();
         ssh_cmd(&remote_host, "insmod /tmp/caliptra-kmod/io_module.ko").unwrap();
         ssh_cmd(&remote_host, "insmod /tmp/caliptra-kmod/rom_backdoor.ko").unwrap();
     }
@@ -171,7 +204,9 @@ fn main() {
     let mut cargo_inv = Command::new("cargo");
     cargo_inv.arg("--config");
     cargo_inv.arg(format!(
-        "target.aarch64-unknown-linux-gnu.rustflags = [\"-C\", \"link-arg=--sysroot={}\"]", sysroot));
+        "target.aarch64-unknown-linux-gnu.rustflags = [\"-C\", \"link-arg=--sysroot={}\"]",
+        sysroot
+    ));
 
     cargo_inv.arg("--config");
     cargo_inv.arg("target.aarch64-unknown-linux-gnu.linker = \"aarch64-linux-gnu-gcc\"");
@@ -221,13 +256,9 @@ fn main() {
         }
         test_files.push(executable.clone().into_std_path_buf());
         println!("{:?}", artifact);
-
     }
     ssh_cmd(&remote_host, "mkdir -p /tmp/fargo-tests").unwrap();
     scp(&test_files, &remote_host, Path::new("/tmp/fargo-tests")).unwrap();
-
-    let home_dir = PathBuf::from(std::env::var("HOME").unwrap());
-    let fargo_dir = home_dir.join(".caliptra-fargo");
 
     let fw_dir = Path::new("/tmp/fargo-fw");
     std::fs::create_dir_all(fw_dir).unwrap();
