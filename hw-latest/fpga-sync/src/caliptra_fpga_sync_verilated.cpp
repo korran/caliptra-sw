61ec42dd767ce729f0206aaea2b5588455568715
Kor Nielsen <kor@google.com>
2023-10-15 20:54:11 -0700

    It compiles

---
+++ hw-latest/fpga-sync/src/caliptra_fpga_sync_verilated.cpp
@@ -0,0 +1,70 @@
+
+#include "caliptra_fpga_sync_verilated.h"
+
+#include "Vcaliptra_fpga_sync_top.h"
+#include "verilated_vcd_c.h"
+
+#include <memory>
+
+struct caliptra_fpga_sync_verilated {
+  Vcaliptra_fpga_sync_top v;
+  std::unique_ptr<VerilatedVcdC> tfp;
+  uint64_t sim_time = 0;
+};
+
+
+struct caliptra_fpga_sync_verilated* caliptra_fpga_sync_verilated_new() {
+  return new caliptra_fpga_sync_verilated();
+}
+
+void caliptra_fpga_sync_verilated_destroy(struct caliptra_fpga_sync_verilated* model) {
+  if (model->tfp.get()) {
+    model->tfp->close();
+  }
+  delete model;
+}
+
+
+void caliptra_fpga_sync_verilated_eval(struct caliptra_fpga_sync_verilated* model,
+                             const struct caliptra_fpga_sync_sig_in* in,
+                             struct caliptra_fpga_sync_sig_out* out) {
+
+  Vcaliptra_fpga_sync_top* v = &model->v;
+  v->eval();
+
+  v->aclk = in->aclk;
+  v->rstn = in->rstn;
+
+  v->arvalid = in->arvalid;
+  v->araddr = in->araddr;
+  v->arprot = in->arprot;
+
+  v->rready = in->rready;
+
+  v->awvalid = in->awvalid;
+  v->awaddr = in->awaddr;
+  v->awprot = in->awprot;
+
+  v->wvalid = in->wvalid;
+  v->wdata = in->wdata;
+  v->wstrb = in->wstrb;
+
+  v->bready = in->bready;
+
+  if (model->tfp.get()) {
+    model->tfp->dump(model->sim_time++);
+  }
+
+  out->arready = v->arready;
+  
+  out->rvalid = v->rvalid;
+  out->rdata = v->rdata;
+  out->rresp = v->rresp;
+
+  out->awready = v->awready;
+
+  out->wready = v->wready;
+
+  out->bvalid = v->bvalid;
+  out->bresp = v->bresp;
+}
---
+++ hw-latest/fpga-sync/src/caliptra_fpga_sync_verilated.h
@@ -0,0 +1,62 @@
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct caliptra_fpga_sync_verilated;
+
+struct caliptra_fpga_sync_sig_in {
+    bool aclk;
+    bool rstn;
+
+    bool arvalid;
+    uint32_t araddr;
+    uint8_t arprot;
+
+    bool rready;
+
+    bool awvalid;
+    uint32_t awaddr;
+    uint8_t awprot;
+
+    bool wvalid;
+    uint64_t wdata;
+    uint8_t wstrb;
+
+    bool bready;
+};
+
+struct caliptra_fpga_sync_sig_out {
+    bool arready;
+
+    bool rvalid;
+    uint64_t rdata;
+    uint8_t rresp;
+
+    bool awready;
+
+    bool wready;
+
+    bool bvalid;
+    uint8_t bresp;
+};
+
+struct caliptra_fpga_sync_verilated* caliptra_fpga_sync_verilated_new();
+void caliptra_fpga_sync_verilated_destroy(struct caliptra_fpga_sync_verilated* model);
+
+void caliptra_fpga_sync_verilated_trace(struct caliptra_fpga_sync_verilated* model,
+                              const char* vcd_out_path, int depth);
+
+// Evaluates the model into out, then copies all `in` signals into psuedo
+// flip-flops that will be visible to always_ff blocks in subsequent
+// evaluations.
+void caliptra_fpga_sync_verilated_eval(struct caliptra_fpga_sync_verilated* model,
+                             const struct caliptra_fpga_sync_sig_in* in,
+                             struct caliptra_fpga_sync_sig_out* out);
+
+#ifdef __cplusplus
+}
+#endif
+
